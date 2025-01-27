7a0dbfc12ab14768e473e9581b4861e2bbcbdcf9
k2gutierrez <cchimal.gutierrez@gmail.com>
2025-01-27 11:01:18 -0600

    added event read

--- src/app/components/zones/Survivor.js
+++ src/app/components/zones/Survivor.js
@@ -11,71 +11,95 @@ import { toBytes } from "viem";
 export default function Survivor() {
 
   const {
-      isConnected,
-      setIsConnected,
-      userAddress,
-      setUserAddress,
-      signer,
-      setSigner,
-      provider,
-      setProvider,
-      location,
-      setLocation,
-      tokenId,
-      setTokenId,
-      isAlive,
-      setIsAlive
-    } = useContext(WalletContext);
+    isConnected,
+    setIsConnected,
+    userAddress,
+    setUserAddress,
+    signer,
+    setSigner,
+    provider,
+    setProvider,
+    location,
+    setLocation,
+    tokenId,
+    setTokenId,
+    isAlive,
+    setIsAlive
+  } = useContext(WalletContext);
+
+  const [loc, setLoc] = useState("")
+  const [id, setId] = useState(0)
+  const [mstatus, setMstatus] = useState()
+  const [lvl, setLvl] = useState(0)
+  const [cstage, setCstage] = useState(0)
+  const [crevive, setCrevive] = useState()
+  const [trigger, setTrigger] = useState(false)
+
+  const [winner, setWinner] = useState(null)
+  let [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    GetUser(tokenId)
+    increase()
+    if (counter > 0){
+      check(tokenId)
+    }
   
-    const [loc, setLoc] = useState("")
-    const [id, setId] = useState(0)
-    const [mstatus, setMstatus] = useState()
-    const [lvl, setLvl] = useState(0)
-    const [cstage, setCstage] = useState(0)
-    const [crevive, setCrevive] = useState()
-    const [trigger, setTrigger] = useState(false)
+  }, [counter])
+
+  function increase() {
+    setTimeout(() => {
+      setCounter(counter + 1)
+    }, 5000);
+    console.log(counter)
     
-    //const [loading, setLoading] = useState(false)
-  
-    useEffect(() => {
-      GetUser(tokenId)
-      if (trigger == true) {
-        check()
-      }
-  
-    }, [trigger])
-  
-    async function check() {
-      let res = await GetUser(tokenId)
-      console.log(res)
-      setTimeout(() => {
-        setIsAlive(mstatus)
-        setLocation(loc)
-      }, 2000);
+  }
+
+  async function check(nft) {
+    if (nft == null) return
+    try {
+      const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
+      const checkWinner = await gameContract.on(gameContract.filters.WinnerSelected, (winner, event) => {
+        setWinner(winner)
+        //console.log(event)
+      })
+      const checkWinnerAgain = await gameContract.queryFilter(
+        gameContract.filters.WinnerSelected,
+        0,
+        'latest'
+      )
+      checkWinnerAgain.forEach(event => {
+        setWinner(event.args[0])
+        console.log(event.args[0])
+      })
+
+    } catch (e) {
+      console.error(e)
     }
-  
-    async function GetUser(nft) {
-      if (nft == null) return
-      try {
-        const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
-        const getUser = await gameContract.getUser(nft)
-        let loc = ethers.decodeBytes32String(getUser[2])
-        setLoc(loc)
-        let id = ethers.toNumber(getUser[0])
-        setId(id)
-        let mStatus = getUser[1]
-        setMstatus(mStatus)
-        let lvl = ethers.toNumber(getUser[3])
-        setLvl(lvl)
-        let Cstage = ethers.toNumber(getUser[4])
-        setCstage(Cstage)
-        let Crevive = getUser[5]
-        setCrevive(Crevive)
-  
-      } catch (e) {
-        console.error(e)
-      }
+  }
+
+  async function GetUser(nft) {
+    if (nft == null) return
+    try {
+      const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
+      const getUser = await gameContract.getUser(nft)
+      let loc = ethers.decodeBytes32String(getUser[2])
+      setLoc(loc)
+      let id = ethers.toNumber(getUser[0])
+      setId(id)
+      let mStatus = getUser[1]
+      setMstatus(mStatus)
+      let lvl = ethers.toNumber(getUser[3])
+      setLvl(lvl)
+      let Cstage = ethers.toNumber(getUser[4])
+      setCstage(Cstage)
+      let Crevive = getUser[5]
+      setCrevive(Crevive)
+
+    } catch (e) {
+      console.error(e)
     }
+  }
 
   return (
     <>
@@ -85,7 +109,16 @@ export default function Survivor() {
       </div>
       <p className="mt-2 text-black text-md font-[family-name:var(--font-hogfish)]">YOU'VE ESCAPED THE BASEMENT PRISON</p>
       <Image className="mt-3" src={"https://d9emswcmuvawb.cloudfront.net/PFP" + tokenId + ".png"} alt="Mingle" width={60} height={60} />
-      <p className="mt-5 mx-10 text-black text-sm font-[family-name:var(--font-PRESSURA)]">Wait for the Raffle</p>
+      { winner == null ?
+        (<p className="mt-5 mx-10 text-black text-sm font-[family-name:var(--font-PRESSURA)]">Wait for the Raffle</p>) :
+        (
+          <>
+            <p className="mt-8 text-black text-md font-[family-name:var(--font-hogfish)]">The winner is!</p>
+            <Image className="mt-3" src={"https://d9emswcmuvawb.cloudfront.net/PFP" + winner + ".png"} alt="Mingle" width={60} height={60} />
+            <p className="mt-1 mx-10 text-black text-sm font-[family-name:var(--font-PRESSURA)]">ID # {winner}</p>
+          </>
+        )
+      }
     </>
   )
 }
