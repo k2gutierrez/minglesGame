d205bb1b896548c928c45c0f5bd9472f21a9be7e
k2gutierrez <cchimal.gutierrez@gmail.com>
2025-01-28 15:56:22 -0600

    added show dead

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -2,9 +2,11 @@
 import Image from "next/image";
 import styles from "./profile.module.css";
 import cls from "classnames";
-import { React, useContext, useState } from 'react';
+import { React, useContext, useState, useEffect } from 'react';
 import { WalletContext } from "../context/wallet";
 import prize from "../../../public/assets/prize.jpg";
+import { ethers } from "ethers";
+import { gameABI } from "../abis/gameABI";
 
 export default function Login({ connect, getMingles }) {
 
@@ -23,6 +25,51 @@ export default function Login({ connect, getMingles }) {
     mingle
   } = useContext(WalletContext);
 
+  const [isPaused, setIsPaused] = useState(null)
+  const [raiding, setRaiding] = useState(null)
+  const [alive, setAlive] = useState(null)
+  const [fallen, setFallen] = useState(null)
+  const [escape, setEscape] = useState(null)
+
+  useEffect(() => {
+    if (isConnected) {
+      check()
+      GetPausedStatus()
+    }
+
+  }, [isConnected])
+
+  async function GetPausedStatus() {
+    try {
+      const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
+      const getPausedStatus = await gameContract.getGamePausedStatus()
+      setIsPaused(getPausedStatus)
+      console.log("function: ", getPausedStatus)
+
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async function check() {
+    try {
+      const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
+      const raiding = await gameContract.getAmountOfRegisteredUsers()
+      console.log(raiding)
+      setRaiding(raiding)
+      const fallen = await gameContract.getCaidos()
+      console.log(fallen)
+      setFallen(fallen)
+      const escape = await gameContract.getSurvivorsLength()
+      console.log(escape)
+      setEscape(escape)
+      setAlive(ethers.toNumber(raiding) - ethers.toNumber(fallen))
+
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return (
     <>
       <div className="grid text-center mt-10">
@@ -33,7 +80,7 @@ export default function Login({ connect, getMingles }) {
           <p className="pt-20 text-black text-xs font-[family-name:var(--font-hogfish)]">MINGLES</p>
           <p className="text-xs text-white font-[family-name:var(--font-pressura)]">Worms Raid</p>
           <p className="px-5 mb-2 text-black text-xs font-[family-name:var(--font-pressura)]">
-            Defeat the GIANT RAVEN 
+            Defeat the GIANT RAVEN
           </p>
           <p className="px-5 mb-2 text-black text-xs font-[family-name:var(--font-pressura)]">
             &
@@ -43,29 +90,48 @@ export default function Login({ connect, getMingles }) {
           </p>
         </div>
       </div>
-      <p className="my-5 text-black text-md font-[family-name:var(--font-hogfish)]">DO YOU ACCEPT THE CALL?</p>
-      {userAddress != null ? (
-        <div className="flex items-center justify-center">
-          <button className={cls(styles.backColor, "my-8 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={getMingles} >Enlist Mingle</button>
-          <button className={cls(styles.backColor, "my-8 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("noMingle")} >Buy Mingle</button>
-        </div>
-      ) : (
-        <div className="flex items-center justify-center">
-          <button className={cls(styles.backColor, "text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={connect} >Yes</button>
-          <button className={cls(styles.backColor, "text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("no")}>No</button>
-        </div>
-      )
+      
+      {alive == 0 && isPaused ?
+        (
+          <div className="grid text-center mt-1">
+            <p className="my-5 text-black text-2xl font-[family-name:var(--font-hogfish)]">The Raven wins</p>
+            <video className="px-5" width="600" height="600" autoPlay loop controls preload="none">
+              <source src="/videos/Dead.mov" />
+              Your browser does not support the video tag.
+            </video>
+            <p className="mt-5 text-black text-md font-[family-name:var(--font-pressura)]">Pathetic.</p>
+            <p className="mt-2 text-black text-md font-[family-name:var(--font-pressura)]">You never stood a chance.</p>
+            <p className="mt-4 text-black text-md font-[family-name:var(--font-pressura)]">Try again... if you dare!</p>
+            <p className="mt-4 mb-5 text-black text-md font-[family-name:var(--font-pressura)]"><span className='text-blue-600 font-[family-name:var(--font-hogfish)]'>WAIT</span> for the game to <span className='text-blue-600 font-[family-name:var(--font-hogfish)]'>RESET</span></p>
+          </div>
+        ) : (
+          userAddress != null ? (
+            <div className="flex items-center justify-center">
+              <button className={cls(styles.backColor, "my-8 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={getMingles} >Enlist Mingle</button>
+              <button className={cls(styles.backColor, "my-8 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("noMingle")} >Buy Mingle</button>
+            </div>
+          ) : (
+            <>
+            <p className="my-5 text-black text-md font-[family-name:var(--font-hogfish)]">DO YOU ACCEPT THE CALL?</p>
+            <div className="flex items-center justify-center">
+              <button className={cls(styles.backColor, "text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={connect} >Yes</button>
+              <button className={cls(styles.backColor, "text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("no")}>No</button>
+            </div>
+            </>
+          )
 
+
+        )
       }
       {isConnected &&
         (
           <>
-          <div className="flex items-center justify-center">
-            <button className={cls(styles.backColor, "my-3 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("rules")} >RULES</button>
-          </div>
-          <div className="flex mb-5 items-center justify-center">
-            <button className={cls(styles.backColor, "my-2 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("board")} >Check the Playerboard</button>
-          </div>
+            <div className="flex items-center justify-center">
+              <button className={cls(styles.backColor, "my-3 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("rules")} >RULES</button>
+            </div>
+            <div className="flex mb-5 items-center justify-center">
+              <button className={cls(styles.backColor, "my-2 text-base mx-5 w-32 p-1 rounded-xl shadow-lg shadow-green-600/20 transition-all hover:shadow-lg hover:shadow-green-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none")} onClick={() => setLocation("board")} >Check the Playerboard</button>
+            </div>
           </>
         )
       }
--- src/app/components/SelectMingle.js
+++ src/app/components/SelectMingle.js
@@ -10,6 +10,8 @@ import { Sign } from "../engine/engine";
 import { GetUser } from "../engine/engine";
 import { hexToString } from "viem";
 import Card from "./Card";
+import { gameABI } from "../abis/gameABI";
+import { ethers } from "ethers";
 
 export default function SelectMingle({ mingle }) {
 
@@ -28,48 +30,84 @@ export default function SelectMingle({ mingle }) {
         setTokenId
     } = useContext(WalletContext);
 
-    //let [token, setToken] = useState(false)
+    const [isPaused, setIsPaused] = useState(null)
+    const [raiding, setRaiding] = useState(null)
+    const [alive, setAlive] = useState(null)
+    const [fallen, setFallen] = useState(null)
+    const [escape, setEscape] = useState(null)
 
-    {/*useEffect(() => {
-        if (mingle) {
-            setToken(true)
-        }
+    useEffect(() => {
+        check()
+        GetPausedStatus()
+    }, [])
 
-    }, [token])
-
-    const getMingleData = async (id) => {
-
-        let type = await GetMingleMetadata(id, provider)
-        let surviveChance = await mingleType(type)
+    async function GetPausedStatus() {
         try {
-            let sign = await Sign(id, surviveChance, signer)
-            //setToken(id)
-            //setTokenId(id)
+            const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
+            const getPausedStatus = await gameContract.getGamePausedStatus()
+            setIsPaused(getPausedStatus)
+            console.log("function: ", getPausedStatus)
+
         } catch (e) {
             console.error(e)
-            window.alert(e)
         }
-    }*/}
+    }
+
+    async function check() {
+        try {
+            const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
+            const raiding = await gameContract.getAmountOfRegisteredUsers()
+            console.log(raiding)
+            setRaiding(raiding)
+            const fallen = await gameContract.getCaidos()
+            console.log(fallen)
+            setFallen(fallen)
+            const escape = await gameContract.getSurvivorsLength()
+            console.log(escape)
+            setEscape(escape)
+            setAlive(ethers.toNumber(raiding) - ethers.toNumber(fallen))
+
+        } catch (e) {
+            console.error(e)
+        }
+    }
 
     return (
         <>
-            <div className="text-center bg-black rounded-xl p-2 mt-10">
-                <p className="text-white font-[family-name:var(--font-pressura)]">Pick your Mingle</p>
-            </div>
-            <div className="text-center grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:gap-8 mt-5 mb-8">
-                { mingle && (
-                    mingle.tokens.map((v, k) => {
+            {alive == 0 && isPaused ? (
+                <div className="grid text-center mt-1">
+                    <p className="my-5 text-black text-2xl font-[family-name:var(--font-hogfish)]">The Raven wins</p>
+                    <video className="px-5" width="600" height="600" autoPlay loop controls preload="none">
+                        <source src="/videos/Dead.mov" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <p className="mt-5 text-black text-md font-[family-name:var(--font-pressura)]">Pathetic.</p>
+                    <p className="mt-2 text-black text-md font-[family-name:var(--font-pressura)]">You never stood a chance.</p>
+                    <p className="mt-4 text-black text-md font-[family-name:var(--font-pressura)]">Try again... if you dare!</p>
+                    <p className="mt-4 text-black text-md font-[family-name:var(--font-pressura)]"><span className='text-blue-600 font-[family-name:var(--font-hogfish)]'>WAIT</span> for the game to <span className='text-blue-600 font-[family-name:var(--font-hogfish)]'>RESET</span></p>
+                </div>
+            ) : (
+                <>
+                    <div className="text-center bg-black rounded-xl p-2 mt-10">
+                        <p className="text-white font-[family-name:var(--font-pressura)]">Pick your Mingle</p>
+                    </div>
+                    <div className="text-center grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:gap-8 mt-5 mb-8">
+                        {mingle && (
+                            mingle.tokens.map((v, k) => {
 
-                        const mingleInfo = v.token.tokenId
-                        
-                        return(
-                            <Card key={k} nft={mingleInfo} />
+                                const mingleInfo = v.token.tokenId
+
+                                return (
+                                    <Card key={k} nft={mingleInfo} />
+                                )
+                            }
+                            )
                         )
-                    }
-                )
-                )
-                }
-            </div>
+                        }
+                    </div>
+                </>
+            )
+            }
         </>
     )
 }
