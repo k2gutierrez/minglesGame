8dbcab1d383f7a9547096d03c29d2405704ae782
k2gutierrez <cchimal.gutierrez@gmail.com>
2025-01-28 19:34:55 -0600

    added the alive onew for board

--- src/app/components/Board.js
+++ src/app/components/Board.js
@@ -31,16 +31,25 @@ export default function Board() {
     const [alive, setAlive] = useState(null)
     const [fallen, setFallen] = useState(null)
     const [escape, setEscape] = useState(null)
-    const [mingles, setMingles] = useState([])
+    const [mingles, setMingles] = useState(null)
+    let [aliveMingles, setAliveMingles] = useState(null)
     let [counter, setCounter] = useState(0)
 
     useEffect(() => {
-        
+
         setTimeout(() => {
             check()
-        }, 1500);
+        }, 1000);
 
     }, [])
+
+    useEffect(() => {
+
+        if (mingles != null){
+            GetUser(mingles)
+        }
+
+    }, [mingles])
 
     async function check() {
         try {
@@ -58,37 +67,29 @@ export default function Board() {
             const survivors = gameContract.getSurvivors().then((value) => {
                 let ar = []
                 for (let i = 0; i < value.length; i++) {
-                    let v = GetUser(value[i])
-                    v.then((v) => {
-                        if (v != 0){
-                            ar.push(v)
-                            
-                        }
-                        setMingles(ar)
-                    })
-                    
+                    ar.push(value[i])
+
                 }
-                
-                
-                
-                })
-                
+                setMingles(ar)
+            })
 
         } catch (e) {
             console.error(e)
         }
     }
 
-    async function GetUser(nft) {
-        if (nft == null) return
+    async function GetUser(mingles) {
+        let arr = []
         const gameContract = new ethers.Contract(process.env.NEXT_PUBLIC_GAME_CONTRACT_SEPOLIA, gameABI, provider)
-        const getUser = await gameContract.getUser(nft)
-        let mStatus = getUser[1]
-        if (mStatus == true){
-            return nft
+        for (let i = 0; i < mingles.length; i++) {
+            const getUser = await gameContract.getUser(mingles[i])
+            let mStatus = getUser[1]
+            if (mStatus == true) {
+                arr.push(getUser[0])
+            }
         }
-        return 0
-        
+        setAliveMingles(arr)
+
     }
 
     return (
@@ -103,20 +104,20 @@ export default function Board() {
                 <p className="text-black font-[family-name:var(--font-pressura)]">Alive: {alive}:</p>
             </div>
             <div className="text-center grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 lg:gap-8 mt-5">
-                {
-                        mingles.map((v, k) => {
+                {aliveMingles != null && (
+                    aliveMingles.map((v, k) => {
 
-                            return (
-                                <div key={k} className="rounded-lg border border-gray-400 font-[family-name:var(--font-hogfish)]">
-                                    <Image src={"https://d9emswcmuvawb.cloudfront.net/PFP" + v + ".png"} alt={v} width={200} height={200} />
-                                    <p className="mt-1">Mingle ID {v}</p>
-                                    <p className="mt-1">{<span className='text-blue-600 font-[family-name:var(--font-hogfish)]'>Alive</span>}</p>
-                                </div>
-                            )
-                        })
+                        return (
+                            <div key={k} className="rounded-lg border border-gray-400 font-[family-name:var(--font-hogfish)]">
+                                <Image src={"https://d9emswcmuvawb.cloudfront.net/PFP" + v + ".png"} alt={v} width={200} height={200} />
+                                <p className="mt-1">Mingle ID {v}</p>
+                                <p className="mt-1">{<span className='text-blue-600 font-[family-name:var(--font-hogfish)]'>Alive</span>}</p>
+                            </div>
+                        )
+                    })
 
+                )
 
-                    
                 }
             </div>
         </>
